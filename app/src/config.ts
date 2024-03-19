e75265da6754cd83f82cf2b0c497bf3fc19856ae
matthewbhinton <163644052+matthewbhinton@users.noreply.github.com>
2024-03-19 16:54:30 -0600

    v

--- app/src/config.ts
+++ app/src/config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
         {
           media: {
             type: 'image',
-            src: import('./assets/img/start1.jpg'),
+            src: import('./assets/img/start.png'),
             size: 800,
           },
           shape: 'square',
